a93287a819b17dd8e19de27431559ee1859e78b4
miila11 <70434220+miila11@users.noreply.github.com>
2020-09-01 14:21:38 +0200

    yquery

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-// diese url abfragen: http://api.weatherstack.com/current?access_key=XXXXXXXXXXX&query=Rotkreuz
+// diese url abfragen: http://api.weatherstack.com/current?access_key=2a42a4fff642fb4f07a7f62b299853a8&query=Rotkreuz
 
 let city = "Lausanne";
-$.getJSON('http://api.weatherstack.com/current?access_key=XXXXXXXXX&query='+city, function(data) {
+$.getJSON('http://api.weatherstack.com/current?access_key=2a42a4fff642fb4f07a7f62b299853a8&query='+city, function(data) {
     let temp = data.current.temperature;
     let uv = data.current.uv_index;
     let icon_url = data.current.weather_icons[0]
