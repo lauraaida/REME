2e548569beedf90a36bb3ceb24c59e8c86d1c091
Gordan Savicic <gordan.savicic@gmail.com>
2020-09-01 15:03:59 +0200

    added countdown script to index.html

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
 // diese url abfragen: http://api.weatherstack.com/current?access_key=2a42a4fff642fb4f07a7f62b299853a8&query=Rotkreuz
 
-let city = "Lausanne";
-$.getJSON('http://api.weatherstack.com/current?access_key=2a42a4fff642fb4f07a7f62b299853a8&query='+city, function(data) {
+let city = "Rotkreuz";
+
+$.getJSON('http://api.weatherstack.com/current?access_key=2a42a4fff642fb4f07a7f62b299853a8&query=' + city, function (data) {
     let temp = data.current.temperature;
     let uv = data.current.uv_index;
     let icon_url = data.current.weather_icons[0]
-    document.querySelector(".wetter_location").innerHTML = "Aktuell " + temp + "°C<br />" + city;
-    document.querySelector(".wetter_icon").innerHTML = "<img src=\"" + icon_url + "\">";
-    document.querySelector(".wetter_uv").innerHTML = "<span class=\"uv_header\">UV-Index</span><span>" + uv + "</span>"; 
+    $(".wetter_location").html("Aktuell " + temp + "°C<br />" + city);
+    $(".wetter_icon").html("<img src=\"" + icon_url + "\">");
+    $(".wetter_uv").html("<span class=\"uv_header\">UV-Index</span><span>" + uv + "</span>");
+});
+
+$(".countdown").countdown({
+    until: +3000,
+    compact: true,
+    format: 'HMS'
 });
